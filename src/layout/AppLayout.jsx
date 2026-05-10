a66492907677813ea3e7f1286a63787e513394e3
Ashfaq Khan <ashfaaq.alam@outlook.com>
2026-05-10 09:24:23 +0530

    Add scroll to top button

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 export const AppLayout = () => {
   return (
@@ -8,6 +9,7 @@ export const AppLayout = () => {
       <Header />
       <Outlet />
       <Footer />
+      <ScrollToTop />
     </>
   );
 };
